6d2c14515c08e6af8bcdb16c1ea44da55acd8292
Swifty1235 <Pedro.romero102008@gmail.com>
2025-01-05 13:52:09 -0500

    trying test

---
+++ Fetch Recipe Test/Fetch_Recipe_Test.swift
@@ -0,0 +1,16 @@
+//
+//  Fetch_Recipe_Test.swift
+//  Fetch Recipe Test
+//
+//  Created by Pedro Romero on 1/5/25.
+//
+
+import Testing
+
+struct Fetch_Recipe_Test {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Fetch Recipe Test/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Fetch Recipe Test
+//
+//  Created by Pedro Romero on 1/5/25.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
